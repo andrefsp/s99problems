3676f8d4c6845aae48815d8645f1812cf3576c2a
andrefsp <andrefsp@gmail.com>
2015-10-07 22:57:49 +0100

    P18 done

--- src/main/scala/com/s99/P17.scala
+++ src/main/scala/com/s99/P17.scala
@@ -4,6 +4,7 @@ package com.s99
 object P17 {
 
   def split(splitIndex: Int, list: List[Any]): (List[Any], List[Any]) = {
-    (List(), List())
+    val partition = list.zipWithIndex.partition((x:(Any, Int)) => x._2 <= splitIndex-1)
+    (partition._1.map((x: (Any, Int)) => x._1), partition._2.map((x: (Any, Int)) => x._1))
   }
 }
